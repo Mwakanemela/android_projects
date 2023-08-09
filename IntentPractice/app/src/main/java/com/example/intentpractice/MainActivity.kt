42ad46114df10ab01ab7af9115cc831af7b66d95
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2023-08-09 18:03:20 +0200

    practiced implicit and explicit intent

--- IntentPractice/app/src/main/java/com/example/intentpractice/MainActivity.kt
+++ IntentPractice/app/src/main/java/com/example/intentpractice/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.intentpractice
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val explicitIntentButton = findViewById<Button>(R.id.explicitIntentButton)
+
+        explicitIntentButton.setOnClickListener {
+            val explicitIntent = Intent(this, SecondActivity::class.java)
+            startActivity(explicitIntent)
+            finish() //so that when user clicks the back button it exit the app
+        }
+
+        //implicit intent
+        var url = "https://github.com/Mwakanemela"
+        val implicitIntentButton = findViewById<Button>(R.id.implicitIntentButton)
+
+        implicitIntentButton.setOnClickListener {
+            val implicitIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(implicitIntent)
+        }
     }
 }
