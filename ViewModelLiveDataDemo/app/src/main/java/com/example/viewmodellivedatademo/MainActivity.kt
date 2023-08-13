f38e4f66bb23738dbcb95af73d7577fc980aa934
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2023-08-13 14:02:27 +0200

    View Model and Live Data practiced - now ready to use for large projects - 6hrs/12hrs half way of the course at app dev notes youtube channel

--- ViewModelLiveDataDemo/app/src/main/java/com/example/viewmodellivedatademo/MainActivity.kt
+++ ViewModelLiveDataDemo/app/src/main/java/com/example/viewmodellivedatademo/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
@@ -19,12 +20,15 @@ class MainActivity : AppCompatActivity() {
         //var count = 0
         //countText.text = count.toString()
         viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
-        countText.text = viewModel.count.toString()
+        //countText.text = viewModel.count.toString()
+        viewModel.count.observe(this, Observer {
+            countText.text = it.toString()
+        })
         countBtn.setOnClickListener {
            // count++
             //countText.text = count.toString()
             viewModel.updateCount()
-            countText.text = viewModel.count.toString()
+            //countText.text = viewModel.count.toString()
         }
     }
 }
