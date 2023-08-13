2236820e532c849db31a9580688051437fb37a98
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2023-08-13 02:45:23 +0200

    Modified first app where i used log.i - for info and demo activity lifecycle

--- FirstApp/app/src/main/java/com/example/firstapp/MainActivity.kt
+++ FirstApp/app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.firstapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
 import android.widget.Button
@@ -14,6 +15,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i("My tag", "On create start")
 
         val greetingTextView = findViewById<TextView>(R.id.tvHello)
         val inputField = findViewById<EditText>(R.id.nameInputField)
@@ -30,7 +33,9 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this@MainActivity,"Please, enter your name", Toast.LENGTH_SHORT).show()
             }else {
                 val message = "Welcome $enteredName"
+                Log.i("My tag", message)
                 greetingTextView.text = message
+
                 inputField.text.clear()
                 offersButton.visibility = VISIBLE
             }
@@ -40,5 +45,35 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("Username", enteredName)
             startActivity(intent)
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("My tag", "MainActivity : onStart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("My tag", "MainActivity : onStop")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("My tag", "MainActivity : onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("My tag", "MainActivity : onPause")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("My tag", "MainActivity : onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("My tag", "MainActivity : onRestart")
     }
 }
--- FirstApp/app/src/main/java/com/example/firstapp/SecondActivity.kt
+++ FirstApp/app/src/main/java/com/example/firstapp/SecondActivity.kt
@@ -1,6 +1,7 @@
 package com.example.firstapp
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -17,6 +18,7 @@ class SecondActivity : AppCompatActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        Log.i("My tag", "SecondActivity : onCreate")
 
         val username = intent.getStringExtra("Username")
         val textView = findViewById<TextView>(R.id.offersTextView)
@@ -24,4 +26,34 @@ class SecondActivity : AppCompatActivity() {
         textView.text = message
     }
 
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("My tag", "SecondActivity : onStart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("My tag", "SecondActivity : onStop")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("My tag", "SecondActivity : onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("My tag", "SecondActivity : onPause")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("My tag", "SecondActivity : onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("My tag", "SecondActivity : onRestart")
+    }
 }
