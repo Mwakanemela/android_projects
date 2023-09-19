b1989606d612cfefd0ea8273c14449ff3dfec329
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2023-09-20 01:48:24 +0200

    I had to add the animations to this fantastic layout

--- RecyclerviewExampleOne/app/src/main/java/com/example/recyclerviewexampleone/nestedrv/ChildAdapter.kt
+++ RecyclerviewExampleOne/app/src/main/java/com/example/recyclerviewexampleone/nestedrv/ChildAdapter.kt
@@ -3,8 +3,11 @@ package com.example.recyclerviewexampleone.nestedrv
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerviewexampleone.R
 
@@ -12,6 +15,8 @@ class ChildAdapter(val childList: List<ChildDataClass>):RecyclerView.Adapter<Chi
     class ViewHolder(itemView:View):RecyclerView.ViewHolder(itemView) {
 
         val imgChild:ImageView = itemView.findViewById(R.id.img_child)
+        val cl:ConstraintLayout = itemView.findViewById(R.id.cl)
+        val cv:CardView = itemView.findViewById(R.id.cv)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -27,5 +32,9 @@ class ChildAdapter(val childList: List<ChildDataClass>):RecyclerView.Adapter<Chi
         val currentItem= childList[position]
 
         holder.imgChild.setImageResource(currentItem.image)
+        holder.cv.startAnimation(
+            AnimationUtils.loadAnimation(
+            holder.cv.context, R.anim.scale_up
+        ))
     }
 }
